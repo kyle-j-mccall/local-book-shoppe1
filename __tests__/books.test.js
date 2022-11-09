b274407396a7f0621bc3af0f63ce103716d6e447
Kyle McCall <kylem147@gmail.com>
2022-11-08 17:31:51 -0800

    /book/:id returns book detail. test passing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -13,6 +13,17 @@ describe('book routes', () => {
     expect(phased).toHaveProperty('title', 'Phased');
     expect(phased).toHaveProperty('released', '2022-05-08T07:00:00.000Z');
   });
+
+  it('/books/:id should return book details', async () => {
+    const res = await request(app).get('/books/1');
+    const expected = {
+      id: '1',
+      title: 'Business-focused',
+      released: '2022-08-09T07:00:00.000Z',
+    };
+
+    expect(res.body).toEqual(expected);
+  });
   afterAll(() => {
     pool.end();
   });
